6469976099a87f5cb21da86f6800d3cd4abc03bd
zhr-nabila <dev.zahranabila@gmail.com>
2025-11-08 20:43:47 +0800

    remove .history folder from tracking

--- .history/routes/web_20251107195752.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CheckController;
-
-Route::get('/', [CheckController::class, 'index'])->name('upload.page');
-Route::post('/process', [CheckController::class, 'process'])->name('process.data');
-Route::post('/download-csv', [CheckController::class, 'downloadCsv'])->name('download.csv');
-Route::post('/download-pdf', [CheckController::class, 'downloadPdf'])->name('download.pdf');
